39a2750f915052013ca1d711c5dc3f5218526592
cdelossantos6136 <cdelossantos_6136@email.ric.edu>
2017-03-22 23:18:13 -0400

    Prototyped guest mode HTML. No functionality.

--- util/HTML, CSS, JS/LostNFound.js
+++ util/HTML, CSS, JS/LostNFound.js
@@ -1,4 +1,8 @@
-function submitForm(){
-	 return submit();
-	
+function myFunction() {
+    var table = document.getElementById("body");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.index = "NEW CELL1";
+    cell2.index = "NEW CELL2";
 }
