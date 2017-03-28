197556b248865d844c8ac4ca708c802853a35cf6
cdelossantos6136 <cdelossantos_6136@email.ric.edu>
2017-03-28 17:34:44 -0400

    Delete some unused HTM related files. added tableExample.html

--- util/HTML, CSS, JS/LostNFound.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var table = document.getElementById("body");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.index = "NEW CELL1";
-    cell2.index = "NEW CELL2";
-}
