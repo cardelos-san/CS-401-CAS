d13e0ca19187e1512ed0417946dc34e02528f16b
Adam Sebzda <asebzda@gmail.com>
2017-03-28 22:15:44 -0400

    Various SQL fixes & added test data

--- util/sql/schema.sql
+++ util/sql/schema.sql
@@ -15,10 +15,10 @@ CREATE TABLE inventory_category_map (
 CREATE TABLE inventory (
     item_id mediumint(8) unsigned NOT NULL auto_increment PRIMARY KEY,
     description text NOT NULL,
-    status enum('lost', 'retrieved'),
+    status enum('lost', 'retrieved') NOT NULL DEFAULT 'lost',
     date_created timestamp NOT NULL default NOW(),
-    date_found DATE NOT NULL,
-    date_retrieved DATE NOT NULL,
+    date_found date NOT NULL,
+    date_retrieved date NULL DEFAULT NULL,
     added_by_user mediumint(8) unsigned NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8  COLLATE=utf8_unicode_ci;
 
@@ -37,7 +37,7 @@ CREATE TABLE retrieval_records (
 CREATE TABLE users (
     user_id mediumint(8) unsigned NOT NULL auto_increment PRIMARY KEY,
     email varchar(50) NOT NULL,
-    password varchar(100),
+    passwd varchar(100),
     role enum('admin', 'user') NOT NULL,
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
@@ -53,6 +53,7 @@ CREATE FULLTEXT INDEX inventory_description    ON inventory (description);
 
 
 -- Foreign key constraints
-ALTER TABLE retrieval_records      ADD CONSTRAINT retrieval_records_item_id_fkey          FOREIGN KEY (item_id)     REFERENCES inventory  (item_id)     ON DELETE NO ACTION ON UPDATE NO ACTION;
-ALTER TABLE inventory_category_map ADD CONSTRAINT inventory_category_map_item_id_fkey     FOREIGN KEY (item_id)     REFERENCES inventory  (item_id)     ON DELETE NO ACTION ON UPDATE NO ACTION;
-ALTER TABLE inventory_category_map ADD CONSTRAINT inventory_category_map_category_id_fkey FOREIGN KEY (category_id) REFERENCES categories (category_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE retrieval_records      ADD CONSTRAINT retrieval_records_item_id_fkey          FOREIGN KEY (item_id)       REFERENCES inventory  (item_id)     ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE inventory_category_map ADD CONSTRAINT inventory_category_map_item_id_fkey     FOREIGN KEY (item_id)       REFERENCES inventory  (item_id)     ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE inventory_category_map ADD CONSTRAINT inventory_category_map_category_id_fkey FOREIGN KEY (category_id)   REFERENCES categories (category_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE inventory              ADD CONSTRAINT inventory_added_by_user_fkey            FOREIGN KEY (added_by_user) REFERENCES users      (user_id)     ON DELETE NO ACTION ON UPDATE NO ACTION;
